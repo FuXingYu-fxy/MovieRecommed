4c4236241b95d6b7f1ecdab90980bed931c70840
Fu Xingyu <1253076882@qq.com>
2022-05-06 10:21:25 +0800

    feat: 完成收藏电影接口

--- src/movie/movie.ts
+++ src/movie/movie.ts
@@ -1,4 +1,5 @@
 import {query} from "@/db"
+import { queryMovieById } from "@/recommend/recommendMovie";
 
 interface MovieRating {
   rating: number;
@@ -18,6 +19,28 @@ export async function updateMovieRating(userId: number, movieId: number, rating:
 export async function queryMovieByTag(tagId: number) {
   const sql = `select movie_id from tag where tag_id=${tagId}`
   const res = await query(sql);
-  console.log(res)
   return res;
+}
+
+export async function addUserFavoriteMovie(userId: number, movieId: number) {
+  const sql = `insert into user_favorite_movie(user_id, movie_id) values(${userId}, ${movieId})`;
+  return await query(sql);
+}
+
+export async function queryFavoriteMovieById(userId: number) {
+  const sql = `select movie_id from user_favorite_movie where user_id=${userId}`;
+  const res = await query<{'movie_id': number}>(sql);
+  if (res.length) {
+    return await queryMovieById(res.map(item => String(item['movie_id'])));
+  }
+  return [];
+}
+export async function queryFavoriteMovie(userId: number, movieId:number) {
+  const sql = `select movie_id from user_favorite_movie where user_id=${userId} and movie_id=${movieId}`;
+  return await query<{'movie_id': number}>(sql);
+}
+
+export async function delUserFavoriteMovie(userId: number, movieId: number) {
+  const sql = `delete from user_favorite_movie where user_id=${userId} and movie_id=${movieId}`;
+  return await query(sql);
 }
--- src/movie/router.ts
+++ src/movie/router.ts
@@ -1,5 +1,5 @@
 import Router from 'koa-router';
-import { queryMovieRating, updateMovieRating } from '@/movie/movie';
+import { addUserFavoriteMovie, delUserFavoriteMovie, queryFavoriteMovie, queryFavoriteMovieById, queryMovieRating, queryUserFavoriteMovie, updateMovieRating } from '@/movie/movie';
 import createMsg from '@/createMsg';
 import { updateUserRating } from '@/recommend/recommendMovie';
 import { queryMovieByTag } from '@/movie/movie';
@@ -41,6 +41,38 @@ movieRouter
       data: await queryMovieByTag(Number(id)),
     });
     await next();
-  });
+  })
+  .get('/queryFavoriteMovieById', async (ctx, next) => {
+    const {id} = ctx.query;
+    ctx.type = 'json';
+    ctx.body = createMsg({
+      data: await queryFavoriteMovieById(Number(id)),
+    })
+    await next();
+  })
+  .post('/addUserFavoriteMovie', async (ctx, next) => {
+    const {userId, movieId} = ctx.request.body;
+    ctx.type = 'json';
+    ctx.body = createMsg({
+      data: await addUserFavoriteMovie(userId, movieId),
+    })
+    await next();
+  })
+  .post('/delUserFavoriteMovie', async (ctx, next) => {
+    const {userId, movieId} = ctx.request.body;
+    ctx.type = 'json';
+    ctx.body = createMsg({
+      data: await delUserFavoriteMovie(userId, movieId),
+    })
+    await next();
+  })
+  .post('/queryFavoriteMovie', async (ctx, next) => {
+    const {userId, movieId} = ctx.request.body;
+    ctx.type = 'json';
+    ctx.body = createMsg({
+      data: await queryFavoriteMovie(userId, movieId),
+    })
+    await next();
+  })
 
 export default movieRouter;
