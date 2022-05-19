489596579813fdea21fcd7943da94e07bb577f10
Fu Xingyu <1253076882@qq.com>
2022-05-19 18:19:20 +0800

    feat: 完成了通过token获取用户信息

--- src/user/router.ts
+++ src/user/router.ts
@@ -1,5 +1,11 @@
 import Router from 'koa-router';
-import { verifyUserByPassword, getToken, verifyToken } from '@/user/verifyUser';
+import {
+  verifyUserByPassword,
+  getToken,
+  verifyToken,
+  registryAccount,
+  queryUserByToken,
+} from '@/user/verifyUser';
 import createMsg from '@/createMsg';
 
 interface LoginResponse {
@@ -11,64 +17,82 @@ interface LoginResponse {
 // TODO 用一种方法让先前的token 过期
 
 const userRouter = new Router();
-
-// 根据token 获取用户信息
-userRouter.get('/user/userinfo', async (ctx, next) => {
-  // TODO 检查token
-  const token = ctx.headers['auth-token'];
-  await next();
-});
 interface LoginBody {
   account: string;
   password: string;
 }
 
-userRouter.post('/user/login', async (ctx, next) => {
-  // 如果有密码, 代表是第一次登陆或者重新登陆
-  const { account, password } = ctx.request.body as LoginBody;
-  if (account && password) {
-    const { pass, data } = await verifyUserByPassword(account, password);
-    if (pass) {
-      // 校验通过, 返回token 流程结束,
-      // 如果pass 通过， 一定存在 id 与 number
-      const token = getToken(data!.id, data!.account);
+// 根据token 获取用户信息
+userRouter
+  .post('/user/info', async (ctx, next) => {
+    const { token } = ctx.request.body;
+    const result = queryUserByToken(token);
+    ctx.body = createMsg({
+      data: {
+        userId: result.id,
+        account: result.account,
+        pass: result.pass,
+      },
+    });
+    await next();
+  })
+  .post('/user/login', async (ctx, next) => {
+    // 如果有密码, 代表是第一次登陆或者重新登陆
+    const { account, password } = ctx.request.body as LoginBody;
+    if (account && password) {
+      const { pass, data } = await verifyUserByPassword(account, password);
+      if (pass) {
+        // 校验通过, 返回token 流程结束,
+        // 如果pass 通过， 一定存在 id 与 number
+        const token = getToken(data!.id, data!.account);
+        ctx.body = createMsg<LoginResponse>({
+          data: {
+            pass,
+            token,
+          },
+        });
+        return await next();
+      }
+      // 校验未通过
       ctx.body = createMsg<LoginResponse>({
         data: {
-          pass,
-          token,
+          pass: false,
+          msg: '账户或密码错误',
         },
       });
       return await next();
     }
-    // 校验未通过
+    // 不存在账号密码, 验证是否有token
+    const token = ctx.headers['auth-token'];
+    if (!token) {
+      ctx.body = createMsg<LoginResponse>({
+        data: {
+          pass: false,
+          msg: '请重新登录',
+        },
+      });
+      return await next();
+    }
+    // 验证token是否有效
+    const { pass, msg } = verifyToken(token as string);
     ctx.body = createMsg<LoginResponse>({
       data: {
-        pass: false,
-        msg: '账户或密码错误'
+        pass,
+        msg,
       },
     });
-    return await next();
-  }
-  // 不存在账号密码, 验证是否有token
-  const token = ctx.headers['auth-token'];
-  if (!token) {
-    ctx.body = createMsg<LoginResponse>({
-      data: {
-        pass: false,
-        msg: '请重新登录'
-      }
-    })
-    return await next();
-  } 
-  // 验证token是否有效
-  const {pass, msg} = verifyToken(token as string);
-  ctx.body = createMsg<LoginResponse>({
-    data: {
-      pass,
-      msg
-    }
+    await next();
   })
-  await next();
-});
+  .post('/user/registry', async (ctx, next) => {
+    const { account, password } = ctx.request.body;
+    if (account && password) {
+      const result = await registryAccount(account, password);
+      ctx.body = createMsg({
+        data: result,
+      });
+      return await next();
+    }
+    await next();
+  });
 
 export default userRouter;
--- src/user/verifyUser.ts
+++ src/user/verifyUser.ts
@@ -6,9 +6,10 @@ interface UserInfo {
   id: number;
   account: string;
 }
-interface UserLoginRequestBody extends UserInfo {
+interface UserQueryResponse extends UserInfo {
   password?: string;
 }
+
 interface Wrap {
   msg?: string;
   pass?: boolean;
@@ -19,17 +20,19 @@ function wrap(options: Wrap) {
     msg: options.msg || '',
     pass: options.pass || false,
     data: options.data || null,
-  }
+  };
 }
 export async function verifyUserByPassword(account: string, password: string) {
   try {
-    const [data] = await query<UserLoginRequestBody>(`select * from user where account = '${account}'`);
+    const [data] = await query<UserQueryResponse>(
+      `select * from user where account = '${account}'`
+    );
     if (data.account === account && data.password === password) {
       delete data.password;
       return wrap({
         pass: true,
-        data
-      })
+        data,
+      });
     } else {
       return wrap({
         msg: '账户或密码错误',
@@ -44,13 +47,17 @@ export async function verifyUserByPassword(account: string, password: string) {
 }
 
 export function getToken(id: number, account: string) {
-  return jwt.sign({
-    id,
-    account
-  }, salt, {
-    // 三天过期
-    expiresIn: '3d',
-  })
+  return jwt.sign(
+    {
+      id,
+      account,
+    },
+    salt,
+    {
+      // 三天过期  '3d'
+      expiresIn: 60,
+    }
+  );
 }
 
 interface VerifyTokenMsg {
@@ -61,12 +68,92 @@ interface VerifyTokenMsg {
 export function verifyToken(token: string): VerifyTokenMsg {
   try {
     // 如果token 非法或者已过期, 就会 throw err
-    jwt.verify(token, salt)
-  } catch  {
+    jwt.verify(token, salt);
+  } catch {
     return {
       pass: false,
-      msg: 'token无效或token已过期, 请重新登录'
+      msg: 'token无效或token已过期, 请重新登录',
+    };
+  }
+  return { pass: true };
+}
+
+interface RegistryResponse {
+  pass: boolean;
+  msg: string;
+  data: {
+    token: string;
+  } | null;
+}
+export async function registryAccount(
+  account: string,
+  password: string
+): Promise<RegistryResponse> {
+  let id: number;
+  const queryCurAcountIsExist = `select id from user where account = '${account}'`;
+  const result = await query(queryCurAcountIsExist);
+  if (result.length) {
+    return {
+      pass: false,
+      msg: '当前账户已存在',
+      data: null,
+    };
+  }
+  // 注册流程
+  // 如果还有未剩余的账户
+  const queryIsSurplus = 'select id from user where account is NULL limit 1';
+  const result2 = await query<{ id: number }>(queryIsSurplus);
+  let pass: boolean = true;
+  if (!result2.length) {
+    // 没有剩余账户了
+    const getId = 'select max(id) as id from user;';
+    const [row] = await query<{ id: number }>(getId);
+    id = row.id + 1;
+    const insertUser = `insert into user(password, account) value('${password}', '${account}')`;
+    try {
+      await query(insertUser);
+    } catch (err: any) {
+      log.danger(`注册用户(insert): ${err.message}`);
+      pass = false;
+    }
+  } else {
+    id = result2[0].id;
+    const updateAccount = `update user set password = '${password}', account = '${account}' where id = ${id}`;
+    try {
+      await query(updateAccount);
+    } catch (err: any) {
+      log.danger(`注册用户(update): ${err.message}`);
+      pass = false;
     }
   }
-  return {pass: true};
+
+  if (!pass) {
+    return {
+      pass,
+      msg: '系统发生错误, 注册失败, 请联系管理员',
+      data: null,
+    };
+  } 
+  const token = getToken(id, account);
+  return {
+    pass,
+    msg: '注册成功, 已为您自动登录',
+    data: {
+      token,
+    },
+  };
+}
+
+
+export function queryUserByToken(token: string) {
+  try {
+    const {id, account} = jwt.verify(token, salt);
+    return {
+      id,
+      account,
+      pass: true
+    }
+  } catch {
+    return {pass: false}
+  }
 }
